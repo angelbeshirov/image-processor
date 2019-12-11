d05d3407c6c1a41e8a4b0f811c035a8991ba267d
ZdravkoGyurov <37419999+ZdravkoGyurov@users.noreply.github.com>
2019-12-11 16:30:42 +0200

    Added login

--- image-processor-ui/src/app/auth/sign-in/sign-in.component.ts
+++ image-processor-ui/src/app/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  credentials = {username: '', password: ''};
+  successMessage: string;
+  errorMessage: string = 'Invalid Credentials';
+  invalidLogin: boolean = false;
+  loginSuccess: boolean = false;
+
+  constructor(private auth: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    this.auth.login(this.credentials.username, this.credentials.password).subscribe(
+      result => {
+        this.invalidLogin = false;
+        this.loginSuccess = true;
+        this.successMessage = "Login successful.";
+        this.router.navigate(['/home']);
+      }, (
+        result => {
+          this.invalidLogin = true;
+          this.loginSuccess = false;
+        }
+      )
+    );
   }
 
 }
